c589811c89d577b9757ce469141b8aa81860948b
4c <4c.mura@gmail.com>
2018-02-12 11:46:45 +0900

    なんとなく

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,234 +1,128 @@
-'use strict';
+"use strict";
 
-var vm = new Vue({
-	el: '#mycounter',
-	data: {
-		count: 0
-	},
-	methods: {
-		countUp: function countUp() {
-			this.count++;
-			changeRotateSpeed();
-		}
-	}
-});
-
-var vm_stop = new Vue({
-	el: '#mystop',
-	methods: {
-		hsStop: function hsStop() {
-			Speed_0();
-		}
-	}
-});
-
-var howManySpinners = 50;
-var scene = new THREE.Scene();
-var box = void 0;
-var controls = void 0;
-var renderer = void 0;
-var camera = void 0;
-var model = [];
-//let model = {};
-var model2 = {};
-var model3 = {};
-var rotate_speed = 0.05;
-var r_radian = 0;
+var WIDTH = 800;
+var HEIGHT = 600;
 var c_radian = 0;
-var geometry = void 0;
-var material = void 0;
+var r_radian = 0;
+var light = void 0;
+var ambient = void 0;
+var hsGeoGruoup;
 
-function renderHandSpinner() {
-	'use strict';
+// レンダラー
+var renderer = new THREE.WebGLRenderer({
+  preserveDrawingBuffer: true
+});
+renderer.setSize(WIDTH, HEIGHT);
+renderer.setClearColor(0xffffff);
+renderer.autoClearColor = false;
+document.body.appendChild(renderer.domElement);
 
-	var light = void 0;
-	var ambient = void 0;
-	var gridHelper = void 0;
-	var axisHelper = void 0;
-	var lightHelp = void 0;
-	var width = 1000;
-	var height = 1000;
-	var modelPath = void 0;
-	var scale_hs = void 0;
+// メイン描画用のシーンとカメラ
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 100);
 
-	//light
-	light = new THREE.DirectionalLight(0xffffff, 1);
-	light.position.set(0, 200, 80);
-	scene.add(light);
-	ambient = new THREE.AmbientLight(0x404040);
-	scene.add(ambient);
+//light
+light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 200, 80);
+scene.add(light);
+ambient = new THREE.AmbientLight(0x404040);
+scene.add(ambient);
 
-	//camera
-	camera = new THREE.PerspectiveCamera(30, width / height, 1, 1000);
-	camera.position.set(0, 400, 300);
-	camera.lookAt(scene.position);
+//背景の定義
+var scene_bg = new THREE.Scene();
+var camera_bg = new THREE.OrthographicCamera(0, WIDTH, HEIGHT, 0, 0, 1000);
+// メイン描画オブジェクト、とりあえずBox
 
-	// helper 現在は非表示
-	//gridHelper = new THREE.GridHelper(200, 50);
-	//scene.add(gridHelper);
-	// axisHelper = new THREE.AxisHelper(1000);
-	// scene.add(axisHelper);
-	//lightHelper = new THREE.DirectionalLightHelper(light , 20)
-	//scene.add(lightHelper);
+var loader = new THREE.JSONLoader();
+var modelPath = '../src/data/hs300k.json';
+// modelPath = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1538236/hs300k.json";
+var model_hs = void 0;
+var model_pn = void 0;
+var mesh;
 
-	//controls
-	controls = new THREE.OrbitControls(camera);
-	//cameraの自動回転
-	controls.autoRotate = true;
-	controls.autoRotateSpeed = 1.5;
+loader.load(modelPath, function (geo, mat) {
+  var geometry = geo;
+  var material = mat;
+  var scale_hs = 1;
+  //let BasicMat = new THREE.MeshBasicMaterial(mat);
+  var BasicMat = new THREE.MeshBasicMaterial(mat);
+  var PhongMat = new THREE.MeshPhongMaterial(mat);
+  BasicMat.color = new THREE.Color("#ffffff");
+  BasicMat.wireframe = true;
 
-	// renderer
-	renderer = new THREE.WebGLRenderer({ antialias: true });
-	renderer.setSize(width, height);
-	renderer.setClearColor(0xffffff);
-	renderer.setPixelRatio(window.devicePixelRatio);
-	document.getElementById('stage').appendChild(renderer.domElement);
+  model_hs = new THREE.Mesh(geo, BasicMat);
+  model_pn = new THREE.Mesh(geo, PhongMat);
 
-	//modelPath = 'src/bear.json';
-	//modelPath = 'src/handspiner_3d.json';
-	//modelPath = '../src/data/handspiner_3d_geo.json';
-	var modelPath1 = './src/data/handspiner_3d_geo.json';
-	var modelPath2 = './src/data/hs3.json';
-	//modelPath = './src/data/hs4.json';
-	//modelPath = './src/data/hs5.json';
-	var modelPath3 = './src/data/hs6.json';
+  model_hs.scale.set(scale_hs, scale_hs, scale_hs);
+  model_pn.scale.set(scale_hs * 1.05, scale_hs * 1.05, scale_hs * 1.05);
+  model_pn.material.color = new THREE.Color("#cccccc");
+  model_hs.material.opacity = 0.01;
+  model_hs.material.transparent = true;
 
-	//let phongMat = new THREE.MeshPhongMaterial(mat);
-	//let phongMat2 = new THREE.MeshPhongMaterial(mat);
-	//let phongMat3 = new THREE.MeshPhongMaterial(mat);
-	//for (let mt of faceMat.materials) {
-	//  mt.color = new THREE.Color(0xffcc88);
-	//}
+  var p_geometry = new THREE.PlaneGeometry(0.3, 0.1, 1);
+  var p_material = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide });
+  var plane = new THREE.Mesh(p_geometry, p_material);
+  plane.rotation.x = 1.6;
+  plane.position.y = 0.6;
+  plane.position.z = 2;
+  // plane.position.x =2;
+
+  hsGeoGruoup = new THREE.Group();
+  // 先ほどのboxをグループに追加
+  hsGeoGruoup.add(plane);
+  //hsGeoGruoup.add(model_pn);
+  hsGeoGruoup.add(model_hs);
+  scene.add(hsGeoGruoup);
+
+  //scene.add(model_hs);　　
+  //scene.add(model_pn);　　
 
 
-	var loader = new THREE.JSONLoader();
+  // var box = new THREE.BoxGeometry(2, 2, 2);
+  // var mat = new THREE.MeshBasicMaterial({
+  //   color: 0xffffff,
+  //   wireframe: true
+  // });
+  // mesh = new THREE.Mesh(box, mat);
+  // scene.add(mesh);
 
-	var _loop = function _loop(j) {
 
-		setTimeout(function () {
-			//modelPath = modelPath2;
-			//scale_hs = 30;
-			if (j % 3 == 0) {
-				modelPath = modelPath1;
-				scale_hs = 0.0002;
-				console.log("hoge1");
-			} else if (j % 3 == 1) {
-				modelPath = modelPath2;
-				scale_hs = 30;
-				console.log("hoge2");
-			} else {
-				modelPath = modelPath3;
-				scale_hs = 5;
-				console.log("hoge3");
-			}
+  // 背景オブジェクト用のシーンとメッシュ
 
-			loader.load(modelPath, function (geo, mat) {
-				geometry = geo;
-				material = mat;
 
-				if (j % 3 == 0) {
-					scale_hs = 0.2;
-				} else if (j % 3 == 1) {
-					scale_hs = 20;
-				} else {
-					scale_hs = 10;
-				}
+  var bg_geometry = new THREE.PlaneGeometry(WIDTH, HEIGHT, 10, 10);
+  var bg_material = new THREE.MeshBasicMaterial({
+    color: 0x000000,
+    transparent: true,
+    opacity: 0.01
+  });
 
-				for (var i = 0; i < howManySpinners; i++) {
-					var phongMat = new THREE.MeshPhongMaterial(mat);
-					model[i] = new THREE.Mesh(geo, phongMat);
+  var bg = new THREE.Mesh(bg_geometry, bg_material);
+  bg.position.x = WIDTH / 2;
+  bg.position.y = HEIGHT / 2;
+  scene_bg.add(bg);
 
-					var randX = 300 * Math.random() - 150;
-					var randY = 400 * Math.random() - 250;
-					var randZ = 600 * Math.random() - 300;
+  //線の描画
+  var maxpoints = 30;
 
-					if (i == 0) {
-						model[i].position.set(0, 20, 0);
-					} else {
-						model[i].position.set(randX, randY, randZ);
-					}
+  main();
+});
 
-					var randColor = 256 * Math.floor(Math.random() * 156) + 255 + 25600;
-					if (i % 2 == 0) {
-						randColor = 256 * Math.floor(Math.random() * 156) + 200 + 25600;
-					}
+function main() {
 
-					if (i % 4 == 0) {
-						randColor = 0x666666;
-					} else if (i % 4 == 1) {
-						randColor = 0xcccccc;
-					} else if (i % 4 == 2) {
-						randColor = 0xaaaaaa;
-					} else {
-						randColor = 0x888888;
-					}
+  r_radian += 0.01;
 
-					//model[i].scale.set(scale, scale, scale);
-					model[i].scale.set(scale_hs, scale_hs, scale_hs);
-					model[i].material.color = new THREE.Color(randColor);
-					model[i].material.opacity = Math.random();
-					model[i].material.transparent = true;
-					scene.add(model[i]);
-				}
-			});
-		}, 2000);
-	};
+  //cameraの位置を設定
+  camera.position.set(0, 7, 0);
+  camera.lookAt({ x: 0, y: 0, z: 0 });
+  // model_hs.rotation.y +=.06;
+  hsGeoGruoup.rotation.y += 5.9 * (2 * 3.14 / 40);
+  // model_hs.position.y += (Math.sin(r_radian) - Math.sin(r_radian-0.01))*8;
+  // model_pn.position.y += (Math.sin(r_radian) - Math.sin(r_radian-0.01))*8;
 
-	for (var j = 0; j < 3; j++) {
-		_loop(j);
-	}
-	render();
+  renderer.render(scene_bg, camera_bg);
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(main);
 }
-
-function addSpinner() {
-	var phongMat = new THREE.MeshPhongMaterial(material);
-	model = new THREE.Mesh(geometry, phongMat);
-	var randX = 800 * Math.random();
-	var randY = 800 * Math.random();
-	var randZ = 800 * Math.random();
-
-	var size = Math.random();
-	model.scale.set(size, size, size);
-	model.position.set(randX, randY, randZ);
-	var randColor = Math.random() * 0xffffff;
-	model.material.color = new THREE.Color(randColor);
-	scene.add(model);
-}
-
-function render() {
-
-	requestAnimationFrame(render);
-	r_radian += 0.01;
-
-	for (var i = 0; i < howManySpinners; i++) {
-		//model[i].rotation.y += rotate_speed;
-		//model[i].position.y += (Math.sin(r_radian) - Math.sin(r_radian-0.01))*150 ;
-
-	}
-
-	c_radian += 0.007;
-	var cameraZ = 150 * Math.sin(c_radian) + 150;
-	// let cameraZ = 0;
-	camera.position.set(0, 600, cameraZ);
-
-	controls.update();
-	renderer.render(scene, camera);
-}
-
-function changeRotateSpeed() {
-	//controls.autoRotateSpeed = vm.count*10;
-	rotate_speed += vm.count * 0.01;
-	for (var i = 0; i < howManySpinners; i++) {
-
-		model[i].rotation.y = 1.8 * vm.count;
-	}
-}
-
-function Speed_0() {
-	vm.count = 0;
-	rotate_speed = 0;
-	//addSpinner();
-}
-
-renderHandSpinner();
 //# sourceMappingURL=script.js.map
--- src/script/script.js
+++ src/script/script.js
@@ -1,241 +1,131 @@
-var vm = new Vue({
-  el: '#mycounter',
-  data: {
-    count: 0
-  },
-  methods: {
-    countUp: function() {
-            this.count++;
-            changeRotateSpeed ();
-      }
-  }
-});
 
-var vm_stop = new Vue({
-  el: '#mystop',
-  methods: {
-    hsStop: function() {
-            Speed_0();
-    }
-  }
-});
-
-let howManySpinners = 50;
-let scene = new THREE.Scene();
-let box;
-let controls;
-let renderer;
-let camera;
-let model = [];
-//let model = {};
-let model2 = {};
-let model3 = {};
-let rotate_speed = 0.05;
-let r_radian = 0;
+var WIDTH = 800;
+var HEIGHT = 600;
 let c_radian = 0;
-let geometry;
-let material;
+let r_radian = 0;
+let light;
+let ambient;
+var hsGeoGruoup;
 
-function renderHandSpinner () {
-  'use strict';
-  let light;
-  let ambient;
-  let gridHelper;
-	let axisHelper;
-  let lightHelp;
-  let width = 1000;
-  let height = 1000;
-	let modelPath ;
-	let scale_hs;
+// レンダラー
+let renderer = new THREE.WebGLRenderer({
+  preserveDrawingBuffer:true
+});
+renderer.setSize( WIDTH, HEIGHT );
+renderer.setClearColor(0xffffff);
+renderer.autoClearColor = false;
+document.body.appendChild(renderer.domElement);
 
-   //light
-  light = new THREE.DirectionalLight(0xffffff, 1);
-  light.position.set(0, 200, 80);
-  scene.add(light);
-  ambient = new THREE.AmbientLight(0x404040);
-  scene.add(ambient);
+// メイン描画用のシーンとカメラ
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 70, WIDTH / HEIGHT, 1, 100 );
 
-	//camera
-  camera = new THREE.PerspectiveCamera(30, width /　height, 1 , 1000);
-  camera.position.set(0, 400, 300);
-  camera.lookAt(scene.position);
+//light
+light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 200, 80);
+scene.add(light);
+ambient = new THREE.AmbientLight(0x404040);
+scene.add(ambient);
 
-  // helper 現在は非表示
-  //gridHelper = new THREE.GridHelper(200, 50);
-  //scene.add(gridHelper);
-  // axisHelper = new THREE.AxisHelper(1000);
-  // scene.add(axisHelper);
-  //lightHelper = new THREE.DirectionalLightHelper(light , 20)
-  //scene.add(lightHelper);
+//背景の定義
+var scene_bg = new THREE.Scene();
+var camera_bg = new THREE.OrthographicCamera(0, WIDTH, HEIGHT, 0, 0, 1000);
+// メイン描画オブジェクト、とりあえずBox
 
-  //controls
-  controls = new THREE.OrbitControls(camera);
-  //cameraの自動回転
-  controls.autoRotate = true;
-  controls.autoRotateSpeed = 1.5;
-
-  // renderer
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(width, height);
-  renderer.setClearColor(0xffffff);
-  renderer.setPixelRatio(window.devicePixelRatio);
-  document.getElementById('stage').appendChild(renderer.domElement);
-
-	//modelPath = 'src/bear.json';
-	//modelPath = 'src/handspiner_3d.json';
-  //modelPath = '../src/data/handspiner_3d_geo.json';
-  let modelPath1 = './src/data/handspiner_3d_geo.json';
-  let modelPath2 = './src/data/hs3.json';
-  //modelPath = './src/data/hs4.json';
-  //modelPath = './src/data/hs5.json';
-  let modelPath3 = './src/data/hs6.json';
-
-	　　　
-	    //let phongMat = new THREE.MeshPhongMaterial(mat);
-	    //let phongMat2 = new THREE.MeshPhongMaterial(mat);
-	    //let phongMat3 = new THREE.MeshPhongMaterial(mat);
-	    //for (let mt of faceMat.materials) {
-	    //  mt.color = new THREE.Color(0xffcc88);
-	  	//}
+let loader = new THREE.JSONLoader();　　
+let modelPath = '../src/data/hs300k.json'
+// modelPath = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1538236/hs300k.json";
+let model_hs;
+let model_pn;
+var mesh;
 
 
+loader.load(modelPath, (geo, mat) => {
+	let geometry = geo;
+	let material = mat;
+	let scale_hs = 1;
+  //let BasicMat = new THREE.MeshBasicMaterial(mat);
+  let BasicMat = new THREE.MeshBasicMaterial(mat);
+  let PhongMat = new THREE.MeshPhongMaterial(mat);
+  BasicMat.color = new THREE.Color("#ffffff");
+  BasicMat.wireframe = true;
+
+  model_hs = new THREE.Mesh(geo, BasicMat);
+  model_pn = new THREE.Mesh(geo, PhongMat);
+
+	model_hs.scale.set(scale_hs, scale_hs, scale_hs);
+	model_pn.scale.set(scale_hs*1.05, scale_hs*1.05, scale_hs*1.05);
+	model_pn.material.color = new THREE.Color("#cccccc");
+	model_hs.material.opacity =0.01;
+	model_hs.material.transparent = true;
+
+	let p_geometry = new THREE.PlaneGeometry( 0.3, 0.1, 1 );
+	let p_material = new THREE.MeshBasicMaterial( {color: 0xff0000, side: THREE.DoubleSide} );
+	let plane = new THREE.Mesh( p_geometry, p_material );
+  plane.rotation.x =1.6;
+  plane.position.y =0.6;
+  plane.position.z =2;
+  // plane.position.x =2;
+
+	hsGeoGruoup = new THREE.Group();
+    // 先ほどのboxをグループに追加
+  hsGeoGruoup.add(plane);
+  //hsGeoGruoup.add(model_pn);
+  hsGeoGruoup.add(model_hs);
+	scene.add( hsGeoGruoup );
+
+	//scene.add(model_hs);　　
+	//scene.add(model_pn);　　
 
 
-	let loader = new THREE.JSONLoader();　　
-
-		 for (let j = 0; j < 3; j++) {
-
-
-
-
-			 setTimeout(()=> {
-			//modelPath = modelPath2;
-			//scale_hs = 30;
-				if (j % 3 == 0 ) {
-					 modelPath = modelPath1;
-					 scale_hs = 0.0002;
-					 console.log("hoge1");
-
-				} else if ( j % 3 == 1) {
-					 modelPath = modelPath2;
-					 scale_hs = 30;
-					 console.log("hoge2");
-				} else {
-					 modelPath = modelPath3;
-					 scale_hs = 5;
-					 console.log("hoge3");
-				}
+  // var box = new THREE.BoxGeometry(2, 2, 2);
+  // var mat = new THREE.MeshBasicMaterial({
+  //   color: 0xffffff,
+  //   wireframe: true
+  // });
+  // mesh = new THREE.Mesh(box, mat);
+  // scene.add(mesh);
 
 
-				loader.load(modelPath, (geo, mat) => {
-					geometry = geo;
-					material = mat;
-
-					if (j % 3 == 0 ) {
-						 scale_hs = 0.2;
-
-					} else if ( j % 3 == 1) {
-						 scale_hs = 20;
-
-					} else {
-						 scale_hs = 10;
-					}
+  // 背景オブジェクト用のシーンとメッシュ
 
 
 
-					for (let i=0; i < howManySpinners; i++ ) {
-				    let phongMat = new THREE.MeshPhongMaterial(mat);
-				    model[i] = new THREE.Mesh(geo, phongMat);
-
-						let randX = 300 * Math.random() - 150;
-						let randY = 400 * Math.random() - 250;
-						let randZ = 600 * Math.random() - 300;
-
-				    if (i == 0) {
-							model[i].position.set(0, 20, 0);
-						} else {
-							model[i].position.set(randX, randY, randZ);
-						}　　
-
-				    let randColor =  256 * Math.floor(Math.random() * 156) + 255 + 25600;
-				    if (i % 2 == 0 ) {
-							randColor =  256 * Math.floor(Math.random() * 156) + 200 + 25600;
-						}
-
-						if (i % 4 == 0 ) {
-							randColor =  0x666666;
-						} else if(i%4==1) {
-							randColor = 0xcccccc;
-						} else if(i % 4==2) {
-							randColor = 0xaaaaaa;
-						} else {
-							randColor = 0x888888;
-						}
-
-						//model[i].scale.set(scale, scale, scale);
-						model[i].scale.set(scale_hs, scale_hs, scale_hs);
-						model[i].material.color = new THREE.Color(randColor);
-						model[i].material.opacity = Math.random();
-						model[i].material.transparent = true;
-						scene.add(model[i]);　　　
-					}
-
-				});
-				}, 2000);
-
-			}
-			render ();
-	}
+  var bg_geometry = new THREE.PlaneGeometry(WIDTH, HEIGHT, 10, 10);
+  var bg_material = new THREE.MeshBasicMaterial({
+    color: 0x000000,
+    transparent: true,
+    opacity: 0.01,
+  });
 
 
-function addSpinner () {
-  let phongMat = new THREE.MeshPhongMaterial(material);
-  model = new THREE.Mesh(geometry, phongMat);
-	let randX = 800 * Math.random();
-	let randY = 800 * Math.random();
-	let randZ = 800 * Math.random();
+  var bg = new THREE.Mesh(bg_geometry, bg_material);
+  bg.position.x = WIDTH/2;
+  bg.position.y = HEIGHT/2;
+  scene_bg.add(bg);
 
-  let size = Math.random();
-	model.scale.set(size, size, size);　　　
-  model.position.set(randX, randY, randZ);
-	let randColor = Math.random() * 0xffffff;　　　
-	model.material.color = new THREE.Color(randColor);
-	scene.add(model);　
+
+	//線の描画
+	var maxpoints = 30;
+
+  main();
+});
+
+
+function main() {
+
+	r_radian += 0.01;
+
+	//cameraの位置を設定
+	camera.position.set(0, 7, 0);
+	camera.lookAt({x:0, y:0, z:0 });
+  // model_hs.rotation.y +=.06;
+   hsGeoGruoup.rotation.y += 5.9*(2*3.14 /40) ;
+	// model_hs.position.y += (Math.sin(r_radian) - Math.sin(r_radian-0.01))*8;
+	// model_pn.position.y += (Math.sin(r_radian) - Math.sin(r_radian-0.01))*8;
+
+  renderer.render( scene_bg, camera_bg );
+  renderer.render( scene, camera );
+
+  requestAnimationFrame(main);
 }
-
-function render () {
-
-  requestAnimationFrame(render);
-  r_radian += 0.01;
-
-	for (let i=0; i < howManySpinners  ; i++ ) {
-  	//model[i].rotation.y += rotate_speed;
-    //model[i].position.y += (Math.sin(r_radian) - Math.sin(r_radian-0.01))*150 ;
-
-	}
-
-	c_radian += 0.007;
-  let cameraZ = 150 * (Math.sin(c_radian)) +150;
- // let cameraZ = 0;
-	camera.position.set(0, 600, cameraZ);
-
-  controls.update();
-  renderer.render(scene, camera);
-}
-
-function changeRotateSpeed () {
-  //controls.autoRotateSpeed = vm.count*10;
- 	rotate_speed += vm.count*0.01;
-  for (let i=0 ; i < howManySpinners; i++) {
-
-		model[i].rotation.y = 1.8*vm.count;
-  }
-}
-
-function Speed_0 () {
-  vm.count = 0;
-  rotate_speed = 0;
- 	//addSpinner();
-}
-
-renderHandSpinner();
